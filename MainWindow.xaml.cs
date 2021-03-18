c3f1fb3aceb5a2ccc6d59f9292c5c01a09133dfd
jerome.clauet <clauet.jerome@hotmail.fr>
2021-03-18 11:12:23 +0100

    JustePrix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace JustePrix
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public int NbrToGuess = new Random().Next(1, 51);
+        public int NbrTry = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string UserInput = TextBoxUserInput.Text;
+            if (int.TryParse(UserInput, out int UserNumber))
+            {
+                if (UserNumber < 1 || UserNumber > 50)
+                {
+                    MessageBox.Show("La valeur saisie n'est pas dans les limites", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    if (UserNumber > NbrToGuess)
+                    {
+                        NbrTry++;
+                        MessageBox.Show("C'est plus petit", "Faux", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    else if (UserNumber < NbrToGuess)
+                    {
+                        NbrTry++;
+                        MessageBox.Show("C'est plus grand", "Faux", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    else if (UserNumber == NbrToGuess)
+                    {
+                        MessageBox.Show("Bravo, vous avez gagné!!\nLe prix a deviner était " + NbrToGuess + ".\nVous avez trouvé en " + NbrTry + " essais.", "Bravo");
+                        ButtonPlay.Visibility = Visibility.Visible;
+                        TextBlockObjectToGuess.Visibility = Visibility.Hidden;
+                        TextBoxUserInput.Visibility = Visibility.Hidden;
+                        ButtonTryNbr.Visibility = Visibility.Hidden;
+                        LabelForUserInput.Visibility = Visibility.Hidden;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("La valeur saisie n'est pas un nombre", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ButtonPlay.Visibility = Visibility.Hidden;
+            TextBlockObjectToGuess.Visibility = Visibility.Visible;
+            TextBoxUserInput.Visibility = Visibility.Visible;
+            ButtonTryNbr.Visibility = Visibility.Visible;
+            LabelForUserInput.Visibility = Visibility.Visible;
+            TextBoxUserInput.Text = ""; 
+            NbrTry = 0;
+            NbrToGuess = new Random().Next(1, 51);
         }
     }
 }
